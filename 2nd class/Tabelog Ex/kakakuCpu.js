3babbbd2680bdffba2e13a5792a7eecfc5bb755d
ShunTakabatake <114969983+ShunTakabatake@users.noreply.github.com>
2022-12-31 14:17:29 +0900

    Updated Tabelog Ex

---
+++ 2nd class/Tabelog Ex/kakakuCpu.js
@@ -0,0 +1,10 @@
+let title = document.querySelectorAll(".menuBoxBtm");
+for(let i = 0; i < title.length; i++){
+  let e = title[i];
+  e.style.display = 'none';
+}
+
+let pr = document.querySelectorAll("#boxPR300, #ct085, #ct086")
+for(let i = 0; i < pr.length; i++){
+  pr[i].style.display = 'none'
+}
